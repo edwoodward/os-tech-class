52e2e8bc8f3356151c403245e948cb0b221e51aa
Ed Woodward <ecw1@rice.edu>
2017-09-20 15:38:51 -0500

    initial checkin

---
+++ examples/python-example.py
@@ -0,0 +1,12 @@
+class Person():
+    def __init__(firstname, lastname, city, state)
+        self.first_name = firstname
+        self.last_name = lastname
+        self.city = city
+        self.state = state
+
+    def get_name():
+        return self.first_name + ' ' + self.last_name
+
+    def get_address():
+        return self.city + ' ' + self.state
